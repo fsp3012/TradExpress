92b2fa86ade726e0965a80d1a6f5f8a1458b5d60
fsp3012 <fakoredeabdulafeezolamide@gmail.com>
2023-04-13 15:16:17 +0100

    arrowIcon

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,18 +1,27 @@
 import React from "react";
+import arrowUp from "../images/arrow-up.png";
+import arrowDown from "../images/arrow-down.png";
 
 const Section2 = ({ data }) => {
   console.log(data);
   return (
     <marquee behavior="scroll" direction="right">
-      <div className="d-flex flex-column flex-md-row  text-center text-md-start sec2 p-3">
+      <div className="d-flex flex-column flex-md-row text-center text-md-start sec2 p-3">
         {data.map((datum) => {
           return (
             <div className=" flex-grow-1">
-              <div className="d-md-flex text-center text-md-start">
+              <div className="d-md-flex gap-3 text-center text-md-start">
                 <p> {datum.symbol}/NGN</p>
                 <p> {datum.percent_change_24h}</p>
+                <span>
+                  {datum.percent_change_24h.startsWith("-") ? (
+                    <img src={arrowDown} alt="" />
+                  ) : (
+                    <img src={arrowUp} alt="" />
+                  )}
+                </span>
               </div>
-              <p className=""> {datum.price_usd}</p>
+              <p> {datum.price_usd}</p>
             </div>
           );
         })}
