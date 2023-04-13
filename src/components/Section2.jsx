d905621b9fd2900df84d6657f7aef08e6f04d7dc
fsp3012 <fakoredeabdulafeezolamide@gmail.com>
2023-04-13 14:17:00 +0100

    marquee fixed

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -3,21 +3,21 @@ import React from "react";
 const Section2 = ({ data }) => {
   console.log(data);
   return (
-    <div className="d-flex flex-column flex-md-row  text-center text-md-start sec2 p-3">
-      {data.map((datum) => {
-        return (
-          <div className=" flex-grow-1">
-            <marquee behavior="scroll" direction="right">
+    <marquee behavior="scroll" direction="right">
+      <div className="d-flex flex-column flex-md-row  text-center text-md-start sec2 p-3">
+        {data.map((datum) => {
+          return (
+            <div className=" flex-grow-1">
               <div className="d-md-flex text-center text-md-start">
                 <p> {datum.symbol}/NGN</p>
                 <p> {datum.percent_change_24h}</p>
               </div>
               <p className=""> {datum.price_usd}</p>
-            </marquee>
-          </div>
-        );
-      })}
-    </div>
+            </div>
+          );
+        })}
+      </div>
+    </marquee>
   );
 };
 
